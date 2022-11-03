6d8c0e460cbad82d9d3a33ec56b5dd15f10812ba
Julio Camargo <jcamargo@hotmail.co.uk>
2022-11-03 15:20:15 +0000

    the first three tests

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 /* 
-👉 Write your kata here!
+CALCULATER - Creat a function that takes two numbers and a mathematical operator and returns the correct value
+
 */
+export function calculator(num1, operator, num2){
+  if(operator == "+"){
+    return num1 + num2
+  }
+  else if(operator == "-"){
+    return num1 - num2
+  }
+  else if(operator == "*"){
+    return num1 * num2
+  }
+  // you code here
+}
 
 //👉 Write the function your CodeWarriors will start with below here:
--- main.test.js
+++ main.test.js
@@ -1,1 +1,31 @@
 //👉 Write your tests below here:
+// test if its a number
+// function calculator(num1, num2, operator)
+// Remember to impot the function 
+import {expect, test} from '@jest/globals';
+import{
+  calculator
+  
+} from './main'
+
+
+test(`This test the plus operator`, () =>{
+  const actual = calculator(2, '+', 2)
+  const expected = 4
+
+  expect(actual).toEqual(expected);
+})
+
+test(`This test the minus operator`, () =>{
+  const actual = calculator(6, '-', 2)
+  const expected = 4
+
+  expect(actual).toEqual(expected);
+})
+
+test(`This test the multiply operator`, () =>{
+  const actual = calculator(6, '*', 2)
+  const expected = 12
+
+  expect(actual).toEqual(expected);
+})
